de2b516d513410b5569fa6e176381ac9aa2a8298
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2024-05-20 12:54:23 +0530

    final commit

--- client/src/services/entityService.jsx
+++ client/src/services/entityService.jsx
@@ -3,31 +3,31 @@ import axios from 'axios';
 
 
 export const createEntity = async (data) => {
-    const response = await axios.post('/entities', data);
+    const response = await axios.post('/api/entities', data);
     return response.data;
   };
   
   export const getEntity = async (entityName) => {
-    const response = await axios.get(`/entities/${entityName}`);
+    const response = await axios.get(`/api/entities/${entityName}`);
     return response.data;
   };
   
   export const getEntries = async (entityName) => {
-    const response = await axios.get(`/entities/${entityName}/entries`);
+    const response = await axios.get(`/api/entities/${entityName}/entries`);
     return response.data;
   };
   
   export const createEntry = async (entityName, entry) => {
-    const response = await axios.post(`/entities/${entityName}/entries`, { entry });
+    const response = await axios.post(`/api/entities/${entityName}/entries`, { entry });
     return response.data;
   };
   
   export const updateEntry = async (entityName, index, updatedEntry) => {
-    const response = await axios.put(`/entities/${entityName}/entries`, { index, updatedEntry });
+    const response = await axios.put(`/api/entities/${entityName}/entries`, { index, updatedEntry });
     return response.data;
   };
   
   export const deleteEntry = async (entityName, entryIndex) => {
-    const response = await axios.delete(`/entities/${entityName}/entries/${entryIndex}`);
+    const response = await axios.delete(`/api/entities/${entityName}/entries/${entryIndex}`);
     return response.data;
   };
